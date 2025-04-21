e2a01d67554c66fad8b8033bdf4bcaf30127c752
thg68 <huuthang682004@gmail.com>
2025-04-22 01:29:00 +0700

    Update NavigationBottomBar giống với của Ngọc

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,198 @@
 import 'package:flutter/material.dart';
-import 'Pages/Home/home.dart';
-import 'Pages/Search/search.dart'; // nhớ đúng tên file nhé
+import 'Pages/Home/home.dart'; // Import trang Home
+import 'Pages/Search/search.dart'; // Import trang Search
+//import 'Pages/Notification/notification.dart'; // Import trang Notification
+//import 'Pages/Profile/profile.dart'; // Import trang Profile
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Từ Điển Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: 'Roboto',
-        scaffoldBackgroundColor: Color(0xFFF9F4FB),
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 254, 247, 255),
       ),
-      home: MainScreen(),
+      home: const MainNavigationScreen(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  int _currentIndex = 0;
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentPageIndex = 0;
 
+  // Danh sách các trang tương ứng với mỗi tab
   final List<Widget> _pages = [
     HomePage(),
     SearchPage(),
-    PlaceholderWidget('Thông Báo'),
-    PlaceholderWidget('Hồ Sơ'),
+    //const NotificationPage(),
+    //const ProfilePage(),
+    PlaceholderWidget("Thông Báo"),
+    PlaceholderWidget("Hồ Sơ"),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.deepPurple,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Trang Chủ',
+      body: _pages[_currentPageIndex],
+      bottomNavigationBar: _buildNavigationBar(),
+    );
+  }
+
+  NavigationBar _buildNavigationBar() {
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        setState(() {
+          _currentPageIndex = index;
+        });
+      },
+      selectedIndex: _currentPageIndex,
+      backgroundColor: const Color(0xFFF3EDF7),
+      destinations: [
+        // Trang chủ
+        NavigationDestination(
+          icon: Container(
+            width: 24,
+            height: 24,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/home_icon.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Tìm Kiếm',
+          label: 'Trang Chủ',
+          selectedIcon: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: ShapeDecoration(
+              color: const Color(0xFFE8DEF8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            child: Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/home_icon.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Thông Báo',
+        ),
+        // Tìm kiếm
+        NavigationDestination(
+          icon: Container(
+            width: 24,
+            height: 24,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/search_icon.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Hồ Sơ',
+          label: 'Tìm Kiếm',
+          selectedIcon: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: ShapeDecoration(
+              color: const Color(0xFFE8DEF8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            child: Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/search_icon.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ),
-        ],
-      ),
+        ),
+        // Thông báo
+        NavigationDestination(
+          icon: Container(
+            width: 24,
+            height: 24,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/notification_icon.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          label: 'Thông Báo',
+          selectedIcon: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: ShapeDecoration(
+              color: const Color(0xFFE8DEF8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            child: Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/notification_icon.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+        // Hồ sơ
+        NavigationDestination(
+          icon: Container(
+            width: 24,
+            height: 24,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/profile_icon.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          label: 'Hồ Sơ',
+          selectedIcon: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: ShapeDecoration(
+              color: const Color(0xFFE8DEF8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            child: Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/profile_icon.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
